a72a582649288d2f31610488ec5b783000dfb070
Mohamad Hammoud <123541524+mkhammoud@users.noreply.github.com>
2024-02-08 22:15:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -27,10 +27,10 @@ st.title("AIDL RAG Chatbot")
 
 
 
-old='''
+
 __import__('pysqlite3') 
 import sys 
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')'''
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 
 
